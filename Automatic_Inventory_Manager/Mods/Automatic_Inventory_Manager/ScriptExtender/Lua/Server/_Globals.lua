5bcee7c6e76c4d453b863845ced176012b89478c
Cameron Meyer <c.meyer95@gmail.com>
2023-11-12 23:35:39 -0600

    Forgot to add some files

---
+++ Automatic_Inventory_Manager/Mods/Automatic_Inventory_Manager/ScriptExtender/Lua/Server/_Globals.lua
@@ -0,0 +1,13 @@
+ITEM_MAP = {
+	['WPN_HUM_LightCrossbow_A_0_43b7fbf5-7f6e-4e9e-bce7-c679eea44593a'] = '',
+	['LOOT_Gold_A'] = '1c3c9c74-34a1-4685-989e-410dc080be6f',
+}
+
+-- Most of this was stolen from Auto_Sell_Loot. Cheers m8 ヾ(⌐■_■)ノ♪
+Config = {
+    initDone = false,
+    config_tbl = { MOD_ENABLED = 1 },
+    config_json_file_path = "config.json",
+    logPath = "log.txt",
+    -- CurrentVersion = Ext.Mod.GetMod(MOD_UUID).Info.ModVersion[1].."."..Ext.Mod.GetMod(MOD_UUID).Info.ModVersion[2].."."..Ext.Mod.GetMod(MOD_UUID).Info.ModVersion[3].."."..Ext.Mod.GetMod(MOD_UUID).Info.ModVersion[4],
+}
